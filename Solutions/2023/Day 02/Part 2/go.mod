c1cab3ab5c3fce9daeadf4b5738ac2697c0d08e3
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2023-12-05 08:01:48 -0700

    Completed Day 2 - Part 2

---
+++ Solutions/2023/Day 02/Part 2/go.mod
@@ -0,0 +1,3 @@
+module github.com/nicholas-fedor/Advent-of-Code/Day-02/Part-2
+
+go 1.21.4
---
+++ Solutions/2023/Day 02/Part 2/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+const (
+	Filename = "input"
+)
+
+type Game struct {
+	Colors map[string]int
+}
+
+func main() {
+	// File handling.
+	file, err := os.Open(Filename)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	// Returns data from file.
+	gameData := parseGameData(file)
+
+	// Solving for Part 2 solution.
+	var sumProduct int
+	for i, games := range gameData {
+		fmt.Printf("Game: %d\n", i+1)
+		var gameProduct int = 1
+		for color, colorCount := range games.Colors {
+			fmt.Printf("%s: %d\n", color, colorCount)
+			gameProduct *= colorCount
+		}
+		fmt.Printf("Game Product: %d\n", gameProduct)
+		fmt.Println(strings.Repeat("-", 15))
+		sumProduct += gameProduct
+	}
+	fmt.Println("Total for all games:", sumProduct)
+}
+
+func parseGameData(file *os.File) []Game {
+	var gamesDataArray []Game
+	gameRegex := regexp.MustCompile(`Game (\d+):(.+)`)
+	scanner := bufio.NewScanner(file)
+
+	var currentGame Game
+	for scanner.Scan() {
+		line := scanner.Text()
+		if gameRegex.MatchString(line) {
+			matches := gameRegex.FindStringSubmatch(line)
+			maxCounts := extractMaxCounts(matches[2])
+			currentGame.Colors = maxCounts
+			gamesDataArray = append(gamesDataArray, currentGame)
+			currentGame = Game{}
+		}
+	}
+	return gamesDataArray
+}
+
+func extractMaxCounts(gameSampleData string) map[string]int {
+	maxCounts := make(map[string]int)
+	samples := strings.Split(gameSampleData, ";")
+	for _, sampleColor := range samples {
+		sampleDataArray := strings.Split(strings.TrimSpace(sampleColor), ",")
+		for _, sampleDataString := range sampleDataArray {
+			sampleData := strings.Split(strings.TrimSpace(sampleDataString), " ")
+			sampleColorCount, _ := strconv.Atoi(sampleData[0])
+			sampleColorName := sampleData[1]
+
+			if sampleColorCount > maxCounts[sampleColorName] {
+				maxCounts[sampleColorName] = sampleColorCount
+			}
+		}
+	}
+	return maxCounts
+}
