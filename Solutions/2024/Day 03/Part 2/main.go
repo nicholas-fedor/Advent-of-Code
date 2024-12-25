9a1b10cc6320549f5ef7d7a1265fcf8a998cef46
Nick Fedor <71477161+nicholas-fedor@users.noreply.github.com>
2024-12-25 00:35:05 -0700

    Add filtering logic

--- Solutions/2024/Day 03/Part 2/main.go
+++ Solutions/2024/Day 03/Part 2/main.go
@@ -41,7 +41,7 @@ func filterData(data []string) [][]int {
 	matches := reMul.FindAllStringSubmatch(dataString, -1)
 	matchesIndex := reMul.FindAllStringSubmatchIndex(dataString, -1)
 	// Output: [[mul(2,4) 2 4] [mul(5,5) 5 5] [mul(11,8) 11 8] [mul(8,5) 8 5]]
-	fmt.Println(matches)
+	fmt.Println("Matches:", matches)
 	// fmt.Println(matchesIndex)
 
 	// Sanitize matchesIndex
@@ -58,16 +58,63 @@ func filterData(data []string) [][]int {
 	// }
 	// fmt.Println(digitPairs)
 
+	// Get regex matches to do() and don't()
 	reDo := regexp.MustCompile("do\\(\\)")
 	reDont := regexp.MustCompile("don't\\(\\)")
 	doIndex := reDo.FindAllStringSubmatchIndex(dataString, -1)
 	dontIndex := reDont.FindAllStringSubmatchIndex(dataString, -1)
-	fmt.Println("doIndex:", doIndex)
-	fmt.Println("dontIndex:", dontIndex)
+
+	// Returns the last index value of each match of reDo and reDont
+	var doIndexSanitized []int
+	for _, i := range doIndex {
+		doIndexSanitized = append(doIndexSanitized, i[1])
+	}
+	var dontIndexSanitized []int
+	for _, i := range dontIndex {
+		dontIndexSanitized = append(dontIndexSanitized, i[1])
+	}
+
+	fmt.Println("doIndex:", doIndexSanitized)
+	fmt.Println("dontIndex:", dontIndexSanitized)
 
 	for _, value := range matchesIndexSanitized {
 		fmt.Println("value:", value)
-		
+
+	}
+
+	// Filtering logic
+	// Iterate through matches index
+	// If first index value in a match is less than the first index value in don't matches, then no need to filter out.
+	// i.e. First match's first index value is 6.
+	// The first index value in the don't() matches is 27.
+	// Ok to proceed with business logic of adding first match to "validMatches" array.
+	// Next match's first index value is 33.
+	// The first don't() match's index value is 27.
+	// Since 33 > 27, this means that value comes after,
+	// and therefore we need to check if there is a do() match index value that is between 27 and 33.
+	// The first (and only) do() match index value is 63, so a false is returned, and this is therefore not a "valid" match.
+	// etc...
+
+	var validMatches [][]int
+	for _, matchIndex := range matchesIndexSanitized {
+		for _, dontMatchIndex := range dontIndexSanitized {
+			fmt.Println("matchIndex:", matchIndex[0], "dontMatchIndex:", dontMatchIndex)
+			if matchIndex[0] < dontMatchIndex {
+				validMatches = append(validMatches, matchIndex)
+			} else if matchIndex[0] > dontMatchIndex {
+				for _, doMatchIndex := range doIndexSanitized {
+					if doMatchIndex < matchIndex[0] {
+						validMatches = append(validMatches, matchIndex)
+					}
+				}
+			}
+		}
+	}
+	fmt.Println("Valid Matches:", validMatches)
+
+	for _, validMatch := range validMatches {
+		// filteredData = append(filteredData, []byte{dataString[validmatch[0]], dataString[validMatch[1]]})
+		fmt.Println(string([]byte{dataString[validMatch[0]-1], dataString[validMatch[1]-1]}))
 	}
 
 	return filteredData
